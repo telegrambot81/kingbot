ce01b541d5c0121b14aca51b7442ef586e0c6666
telegrambot81 <telegrambot81@gmail.com>
2016-03-31 13:19:40 +0430

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local sudo = 118682430
+local sudo = 106789762
     if matches[1]:lower() == "مدیرو بیار" then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
     end
